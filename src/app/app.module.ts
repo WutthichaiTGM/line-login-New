71c0549cc4f2e0969d90dfd8f26b1676e2e0c520
WutthichaiTGM <wutthichai.sri.60@ubu.ac.th>
2020-09-28 08:18:22 +0700

    1 file modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environment/environment';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, AngularFireModule.initializeApp(environment.firebaseApp), ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
---
+++ src/environment/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: false,
+  firebaseApp: {
+    apiKey: 'AIzaSyAIqohg5_f_VBJkNydAN_fDcZIU4kexOBM',
+    authDomain: 'incubate-internship-2020.firebaseapp.com',
+    databaseURL: 'https://incubate-internship-2020.firebaseio.com',
+    projectId: 'incubate-internship-2020',
+    storageBucket: 'incubate-internship-2020.appspot.com',
+    messagingSenderId: '737851649336',
+    appId: '1:737851649336:web:8456a2de3d168ac62b25cd',
+    measurementId: 'G-H2QLLHPJ7Q'
+  },
+  liffId: '1654905366-pdX7j1KM'
+};
